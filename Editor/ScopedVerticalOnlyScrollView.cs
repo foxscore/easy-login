e1b23f31b0a0a8f4bf5feab2068f59575ac5b35f
Felix Kaiser <felixkaiser2000@protonmail.com>
2025-06-19 15:58:48 +0200

    Added a scroll-bar to the account selection view

---
+++ Editor/ScopedVerticalOnlyScrollView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Foxscore.EasyLogin
+{
+    public class ScopedVerticalOnlyScrollView : IDisposable
+    {
+        public Vector2 ScrollPosition { get; private set; }
+        
+        public ScopedVerticalOnlyScrollView(Vector2 scrollPosition)
+        {
+            ScrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUIStyle.none, GUI.skin.verticalScrollbar);
+        }
+        
+        public void Dispose()
+        {
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
