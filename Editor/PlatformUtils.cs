cd17b0b177b0206f21c351e7e5441b213855dff8
Felix Kaiser <felixkaiser2000@protonmail.com>
2025-09-10 01:27:34 +0200

    Made ARM detection universal

--- Editor/PlatformUtils.cs
+++ Editor/PlatformUtils.cs
@@ -1,36 +1,38 @@
+using System.Runtime.InteropServices;
+using HarmonyLib;
+using UnityEditor;
+using UnityEngine;
+
 namespace Foxscore.EasyLogin
 {
     public static class PlatformUtils
     {
         private static bool? _isPlatformSupported;
+
         public static bool IsPlatformSupported()
         {
-#if !UNITY_EDITOR_OSX
-            return true;
-#else
             if (_isPlatformSupported is not null)
                 return _isPlatformSupported.Value;
+
+            if (
+                RuntimeInformation.ProcessArchitecture is not Architecture.Arm and not Architecture.Arm64 ||
+                typeof(Harmony).Assembly.GetName().Version is { Major: >= 2, Minor: >= 4 }
+            )
+                return (_isPlatformSupported = true).Value;
+
+            if (SessionState.GetBool("EasyLogin::ArmPlatformCheck::DisplayedWarningMessage", false))
+                return (_isPlatformSupported = false).Value;
             
-            if (RuntimeInformation.ProcessArchitecture is Architecture.Arm or Architecture.Arm64)
-            {
-                _isPlatformSupported = false;
-                if (SessionState.GetBool("EasyLogin::MacArmPlatformCheck::DisplayedWarningMessage", false) == false)
-                {
-                    SessionState.SetBool("EasyLogin::MacArmPlatformCheck::DisplayedWarningMessage", true);
-                    EditorApplication.delayCall += () => EditorUtility.DisplayDialog(
-                        "Unsupported platform",
-                        "You are attempting to use the ARM version of Unity on a mac.\n\nDue to the Harmony library not supporting this yet, neither Easy Login nor many other packages will work properly.\n\nTry using the non-ARM version of Unity to possibly circumvent this issue.",
-                        ok: "Close"
-                    );
-                }
-            }
-            else
-            {
-                _isPlatformSupported = true;
-            }
-            
-            return _isPlatformSupported.Value;
-#endif
+            SessionState.SetBool("EasyLogin::ArmPlatformCheck::DisplayedWarningMessage", true);
+            EditorApplication.delayCall += () => EditorUtility.DisplayDialog(
+                "Unsupported platform",
+                "You are attempting to use the ARM version of Unity.\n\n" +
+                "Due to the currently installed version of the Harmony library not supporting this yet, neither Easy Login nor many other packages will work properly.\n\n" +
+                "Try using the non-ARM version of Unity to possibly circumvent this issue, or replacing the Harmony version VRChat is providing with your own and restarting Unity.\n\n" +
+                $"Installed: {typeof(Harmony).Assembly.GetName().Version}\nMinimum: 2.4.0.0",
+                ok: "Close"
+            );
+            return (_isPlatformSupported = false).Value;
         }
     }
 }
