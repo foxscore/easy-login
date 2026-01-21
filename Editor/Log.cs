6012c12c687fd19531a685edeeffe6ddf7c31d69
Felix Kaiser <felixkaiser2000@protonmail.com>
2026-01-21 20:05:39 +0100

    Added calling-type detection to logs

--- Editor/Log.cs
+++ Editor/Log.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -83,32 +84,78 @@ namespace Foxscore.EasyLogin
             }
         }
         
+        // Internal calls
         private const string UnityDebugPrefix =
             "<color=grey>[</color><b><color=cyan>Easy Login</color></b><color=grey>]</color> ";
-
-        public static void Debug(string message, Exception exception = null)
+        private static void Internal_Debug(Type type, string message, Exception exception)
         {
 #if FOXY_DEBUG
             UnityEngine.Debug.Log("<color=grey>[<b>Easy Login</b>]</color> " + message);
 #endif
-            WriteToLogFile("DBG", message, exception);
+            WriteToLogFile("DBG", $"[{type?.Name ?? "ERR_NoType"}] {message}", exception);
         }
-
-        public static void Info(string message, Exception exception = null)
+        private static void Internal_Info(Type type, string message, Exception exception)
         {
             UnityEngine.Debug.Log(UnityDebugPrefix + message);
-            WriteToLogFile("INF", message, exception);
+            WriteToLogFile("INF", $"[{type?.Name ?? "ERR_NoType"}] {message}", exception);
+        }
+        private static void Internal_Warning(Type type, string message, Exception exception)
+        {
+            UnityEngine.Debug.Log(UnityDebugPrefix + message);
+            WriteToLogFile("WRN", $"[{type?.Name ?? "ERR_NoType"}] {message}", exception);
+        }
+        private static void Internal_Error(Type type, string message, Exception exception)
+        {
+            UnityEngine.Debug.Log(UnityDebugPrefix + message);
+            WriteToLogFile("ERR", $"[{type?.Name ?? "ERR_NoType"}] {message}", exception);
+        }
+        
+        // Type-fetching calls
+        public static void Debug(string message, Exception exception = null)
+        {
+            var callerType = new StackTrace()
+                .GetFrame(1)
+                .GetMethod()
+                .DeclaringType;
+            while (true == callerType?.IsDefined(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), inherit: false))
+                callerType = callerType.DeclaringType;
+            Internal_Debug(callerType, message, exception);
+        }
+        public static void Info(string message, Exception exception = null)
+        {
+            var callerType = new StackTrace()
+                .GetFrame(1)
+                .GetMethod()
+                .DeclaringType;
+            while (true == callerType?.IsDefined(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), inherit: false))
+                callerType = callerType.DeclaringType;
+            Internal_Info(callerType, message, exception);
         }
         public static void Warning(string message, Exception exception = null)
         {
-            UnityEngine.Debug.LogWarning(UnityDebugPrefix + message);
-            WriteToLogFile("WRN", message, exception);
+            var callerType = new StackTrace()
+                .GetFrame(1)
+                .GetMethod()
+                .DeclaringType;
+            while (true == callerType?.IsDefined(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), inherit: false))
+                callerType = callerType.DeclaringType;
+            Internal_Warning(callerType, message, exception);
         }
-
         public static void Error(string message, Exception exception = null)
         {
-            UnityEngine.Debug.LogError(UnityDebugPrefix + message);
-            WriteToLogFile("ERR", message, exception);
+            var callerType = new StackTrace()
+                .GetFrame(1)
+                .GetMethod()
+                .DeclaringType;
+            while (true == callerType?.IsDefined(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), inherit: false))
+                callerType = callerType.DeclaringType;
+            Internal_Error(callerType, message, exception);
         }
+        
+        // Typed calls
+        public static void Debug<T>(string message, Exception exception = null) => Internal_Debug(typeof(T), message, exception);
+        public static void Info<T>(string message, Exception exception = null) => Internal_Info(typeof(T), message, exception);
+        public static void Warning<T>(string message, Exception exception = null) => Internal_Warning(typeof(T), message, exception);
+        public static void Error<T>(string message, Exception exception = null) => Internal_Error(typeof(T), message, exception);
     }
 }
